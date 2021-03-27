ddb50cf14ac78d63b918209558a9e62a321d80b2
Asta Svirušienė <81101264+AstaSvirusiene@users.noreply.github.com>
2021-03-27 16:40:12 +0200

    Create getTea.js

---
+++ Functional Programming/Learn About: Functional Programming/getTea.js
@@ -0,0 +1,24 @@
+/* Use destructuring assignment with the rest parameter to perform an effective Array.prototype.slice() 
+so that arr is a sub-array of the original array source with the first two elements omitted. */
+
+// Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
