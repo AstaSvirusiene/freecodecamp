3b53c566460f514f41f3aa0324bea31876146d0a
Asta Svirušienė <81101264+AstaSvirusiene@users.noreply.github.com>
2021-04-08 21:46:07 +0300

    Update source.js

--- ES6/Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements/source.js
+++ ES6/Use Destructuring Assignment with the Rest Parameter to Reassign Array Elements/source.js
@@ -4,7 +4,7 @@ so that arr is a sub-array of the original array source with the first two eleme
 const source = [1,2,3,4,5,6,7,8,9,10];
 function removeFirstTwo(list) {
   // Only change code below this line
-  const arr = list; // Change this line
+  const [a, b, ...arr] = list; // Change this line
   // Only change code above this line
   return arr;
 }
